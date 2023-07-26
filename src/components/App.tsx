b67fcb4ac639bbfa72f24766680cf5382deedd6e
oskartyrala <oskartyrala@protonmail.com>
2023-07-26 12:10:45 +0200

    Clean up template files

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,8 @@
-import { MyComponent } from "./MyComponent";
 import "./App.css";
-import { greet } from "../greet";
 
 function App() {
     return (
-        <div className="App">
-            <MyComponent />
-
-            {greet("World")}
-        </div>
+        <></>
     );
 }
 
